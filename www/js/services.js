ebfd5125dbf34cafaa83a65b1caa26057333a0e7
PetronellaSimonsbacka <gussimonpe@student.gu.se>
2016-10-10 18:21:57 +0200

    Added restaurant title to js form

--- www/js/services.js
+++ www/js/services.js
@@ -10,35 +10,40 @@ angular.module('starter.services', [])
     lastText: 'Your mum could not cook it better.',
     face: 'img/ben.png',
     place: 'img/restaurant1.jpg',
-    moreText: 'BETTER at Avenyn 22, open 12.00-00.00.'
+    moreText: 'BETTER at Avenyn 22, open 12.00-00.00.',
+    restaurant: "BETTER"
   }, {
     id: 1,
     name: 'Sushi',
     lastText: 'Asian food, fresh and crispy.',
     face: 'img/max.png',
     place: 'img/restaurant2.jpg',
-    moreText: 'SUSHI FOR BEGINNERS at Andra Långgatan 22, open 18.00-02.00.'
+    moreText: 'SUSHI FOR BEGINNERS at Andra Långgatan 22, open 18.00-02.00.',
+    restaurant: "SUSHI FOR BEGINNERS"
   }, {
     id: 2,
     name: 'Vegan',
     lastText: 'Why eat animals when vegetables can taste like this?',
     face: 'img/adam.jpg',
     place: 'img/restaurant3.jpg',
-    moreText: 'VEGGIELOVER at Nordhemsgatan 9 22, open 20.00-22.00.'
+    moreText: 'VEGGIELOVER at Nordhemsgatan 9 22, open 20.00-22.00.',
+    restaurant: "VEGGIELOVER"
   }, {
     id: 3,
     name: 'Hamburgers',
     lastText: 'Fast food has never tested better',
     face: 'img/perry.png',
     place: 'img/restaurant4.jpg',
-    moreText: 'BIG BAD BURGER at Paternostergatan 24, open 08.00-02.00.'
+    moreText: 'BIG BAD BURGER at Paternostergatan 24, open 08.00-02.00.',
+    restaurant: "BIG BAD BURGER"
   }, {
     id: 4,
     name: 'Just Desserts',
-    lastText: 'This is wicked good ice cream.',
+    lastText: 'This is wicked good dessert.',
     face: 'img/mike.png',
     place: 'img/restaurant5.jpg',
-    moreText: 'SWEEEETIE at Fjällgatan, open 13.00-19.00.'
+    moreText: 'SWEEEETIE at Fjällgatan, open 13.00-19.00.',
+    restaurant: "SWEEEETIE"
   }];
 
   return {
