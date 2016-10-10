c9a4cdb3becda5020aea31ab4fa0694962367243
PetronellaSimonsbacka <gussimonpe@student.gu.se>
2016-10-10 17:51:29 +0200

    Linked in restaurants to js form

--- www/js/services.js
+++ www/js/services.js
@@ -8,27 +8,32 @@ angular.module('starter.services', [])
     id: 0,
     name: 'Traditional Home Cooking',
     lastText: 'Your mum could not cook it better.',
-    face: 'img/ben.png'
+    face: 'img/ben.png',
+    place: 'img/restaurant1.jpg'
   }, {
     id: 1,
     name: 'Sushi',
     lastText: 'Asian food, fresh and crispy.',
-    face: 'img/max.png'
+    face: 'img/max.png',
+    place: 'img/restaurant2.jpg'
   }, {
     id: 2,
     name: 'Vegan',
     lastText: 'Why eat animals when vegetables can taste like this?',
-    face: 'img/adam.jpg'
+    face: 'img/adam.jpg',
+    place: 'img/restaurant3.jpg'
   }, {
     id: 3,
     name: 'Hamburgers',
     lastText: 'Fast food has never tested better',
-    face: 'img/perry.png'
+    face: 'img/perry.png',
+    place: 'img/restaurant4.jpg'
   }, {
     id: 4,
     name: 'Just Desserts',
     lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
+    face: 'img/mike.png',
+    place: 'img/restaurant5.jpg'
   }];
 
   return {
